901390551647c7d37f4208b190cba8b14bc9aaf1
sayjeyhi <bomber.man87@yahoo.com>
2021-04-28 22:10:54 +0000

    Deploying to gh-pages from  @ c4ccb24e74c1b44b2f90561fa61cd4dcc697d704 🚀

--- _next/static/tEs9dGUPo1rUQZpkzD3Nq/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002F27175f2b038d3b1dd39d.css","static\u002Fchunks\u002Fpages\u002Findex-3fe63b53a3fa6f4f4756.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-0b4f33da6e3387894817.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
